cdc4875a77c00a5e84bd75554c790b518ab676cb
Chokoet <1705890@students.wits.ac.za>
2019-08-11 02:11:15 +0200

    Exercise 2.1

--- src/vector/size_capacity.cpp
+++ src/vector/size_capacity.cpp
@@ -20,3 +20,13 @@ int main()
 
     return 0;
 }
+
+//Exercise 2.1
+
+//In line 4: vec: size: 0
+//In line 5: capacity: 0
+
+//A size of a vector can be defined as the total number of elements in a vector.
+//While the capacity is the maximum number of elements a vector can store before resizing.
+
+//The vector's capacity doubles in order to store more elements. This occurs when the capacity is full and a new elements needs to be added to maximize space efficiency.
